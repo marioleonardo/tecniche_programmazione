50b1a5b0932b09427639cfd5f0fd88ba717f6c86
mario <mario.attrovio@gmail.com>
2021-03-03 10:03:14 +0100

    start

---
+++ cmake-build-debug/CMakeFiles/Tecniche_programmazione.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Tecniche_programmazione.dir/lab1/main.c.o"
+  "Tecniche_programmazione"
+  "Tecniche_programmazione.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Tecniche_programmazione.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ lab1/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+
+int main() {
+    int ciao=4;
+    printf("Hello, World!\n");
+    return 0;
+}
